e34d601bd459cbebf26f9db893412dbeb58224b7
Anna-Gevorgian <147203309+Anna-Gevorgian@users.noreply.github.com>
2025-10-03 21:07:47 +0400

    Add files via upload

---
+++ consumer.py
@@ -0,0 +1,31 @@
+import boto3
+
+
+QUEUE_URL = "https://sqs.eu-north-1.amazonaws.com/985124894153/MyQueue"
+
+sqs = boto3.client(
+    "sqs",
+    region_name="eu-north-1",
+    aws_access_key_id="AKIA6KXQE6XEXP2JDI74",
+    aws_secret_access_key="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",
+)
+
+sqs.set_queue_attributes(
+    QueueUrl=QUEUE_URL,
+    Attributes={
+        "VisibilityTimeout": "0"  # in seconds
+    }
+)
+
+# Send a message
+# sqs.send_message(QueueUrl=QUEUE_URL, MessageBody="Hello from Python")
+
+if __name__ == '__main__':
+    resp = sqs.receive_message(QueueUrl=QUEUE_URL, MaxNumberOfMessages=1, WaitTimeSeconds=5)
+
+    if "Messages" in resp:
+        msg = resp["Messages"][0]
+        print("Received:", msg["Body"])
+        sqs.delete_message(QueueUrl=QUEUE_URL, ReceiptHandle=msg["ReceiptHandle"])
+    else:
+        print("No messages available")
---
+++ producer.py
@@ -0,0 +1,20 @@
+import boto3
+
+
+sqs = boto3.client(
+    "sqs",
+    region_name="eu-north-1",
+    aws_access_key_id="AKIA6KXQE6XEXP2JDI74",
+    aws_secret_access_key="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",
+)
+
+
+queue_url = "https://sqs.eu-north-1.amazonaws.com/985124894153/MyQueue"
+
+if __name__ == '__main__':
+    response = sqs.send_message(
+        QueueUrl=queue_url,
+        MessageBody='Hello from Python!'
+    )
+
+    print("Message ID:", response['MessageId'])
